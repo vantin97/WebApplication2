a31da83fff13097587b8e11dfe981b497afd923c
rice <tonthatanhminhktpm@gmail.com>
2020-07-29 11:23:12 +0700

    mkoko

--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -15,6 +15,7 @@ namespace WebApplication2
 {
     public class Startup
     {
+        //ajdiashsdiashsdagbgduiahdhuahdkasdajksdhwaiudbajkd
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
